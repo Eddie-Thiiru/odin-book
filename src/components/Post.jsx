633b139d40ffbf5bb6e2b46741fa2c7ead4bb054
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-10-11 07:04:02 +0300

    Add comment dropdown to posts
    This displays currents post comments and also a form where the
    user can write and submit comments,

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,55 @@
+import { useState } from "react";
 import "../stylesheets/post.css";
 
 const Comment = () => {
   return (
-    <div className="createComment">
+    <div className="comment">
       <img src="" alt="" />
-      <form className="commentForm">
-        <label>
-          <input
-            type="textarea"
-            className="commentInput"
-            name="text"
-            placeholder="Write a comment"
-          />
-        </label>
-        <button type="submit">Add comment</button>
-      </form>
+      <div className="commentUserWrapper">
+        <h4>Example name</h4>
+        <p>Example comment</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentSection = () => {
+  return (
+    <div className="commentsSection">
+      <div className="commentFormContainer">
+        <img src="" alt="" />
+        <form className="commentForm">
+          <label>
+            <input
+              type="textarea"
+              className="commentInput"
+              name="text"
+              placeholder="Write a comment"
+            />
+          </label>
+          <button type="submit" className="submitCommentBtn">
+            Add
+          </button>
+        </form>
+      </div>
+      <div className="commentsContainer">
+        <Comment />
+      </div>
     </div>
   );
 };
 
 const Post = () => {
+  const [commentsOpen, setCommentsOpen] = useState(false);
+
+  const openCommentsSection = () => {
+    setCommentsOpen(true);
+  };
+
+  // const closeCommentsSection = () => {
+  //   setCommentsOpen(false);
+  // };
+
   return (
     <div className="post">
       <header className="postHeader">
@@ -37,25 +67,28 @@ const Post = () => {
         {/* if there is an image create image */}
       </div>
       <footer className="postFooter">
-        <div>
-          {/* if only 1 like display as "like" else likes */}
-          <p>0 likes</p>
-          {/* if only 1 comment display as "comment"  else "comments*/}
-          <p>0 comments</p>
+        <div className="postFooterOne">
+          <div>
+            {/* if only 1 like display as "like" else likes */}
+            <p>0 likes</p>
+            {/* if only 1 comment display as "comment"  else "comments*/}
+            <p>0 comments</p>
+          </div>
+          <div>
+            <button type="button" className="likeBtn">
+              Like
+            </button>
+            <button
+              type="button"
+              className="commentBtn"
+              onClick={openCommentsSection}
+            >
+              Comment
+            </button>
+          </div>
         </div>
-        <div>
-          <button type="button" className="likeBtn">
-            Like
-          </button>
-          <button type="button" className="commentBtn">
-            Comment
-          </button>
-        </div>
+        {commentsOpen === true && <CommentSection />}
       </footer>
-      {/* 
-        When comment button clicked show comment form
-        <Comment />
-      */}
     </div>
   );
 };
